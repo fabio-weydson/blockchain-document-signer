1313216b2a9c00b9f75b8cf76f5aaf069c4d9d9e
Fabio Weydson <fabioweydson@gmail.com>
2025-09-23 18:54:16 -0300

    style: modal backdrop color

--- src/components/documents/DocumentViewModal.tsx
+++ src/components/documents/DocumentViewModal.tsx
@@ -21,7 +21,7 @@ export default function DocumentViewModal({
 
   return (
     <div id="document-details-modal">
-      <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
+      <div className="fixed inset-0 bg-black/50 bg-opacity-40 flex items-center justify-center z-50">
         <div className="bg-white dark:bg-gray-900 rounded-lg shadow-lg p-6 w-full max-w-lg relative">
           <button
             className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 cursor-pointer"
