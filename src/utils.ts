73035204d52f53fb2021f80d58533a0a01e5c63e
Fabio Weydson <fabioweydson@gmail.com>
2025-09-23 18:51:10 -0300

    feat: function to generate doc slug

--- src/utils.ts
+++ src/utils.ts
@@ -7,4 +7,14 @@ const formatHash = (
   return `${address.slice(0, atStart)}...${address.slice(-atEnd)}`;
 };
 
-export { formatHash };
+const docNameSlugify = (hash: string, name: string) => {
+  const hashPart = hash.slice(0, 8);
+  const extensionPart = name.includes(".") ? name.split(".").pop() : "";
+  const namePart = name
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/(^-|-$)+/g, "");
+  return `${hashPart}-${namePart}.${extensionPart}`;
+};
+
+export { formatHash, docNameSlugify };
